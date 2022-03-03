e22987b60b10450368305469ba967df0d0be4e39
Andy Ward <andy@thewardhouse.net>
2022-03-03 13:18:27 +0000

    Add project files.

---
+++ Xbim.Demo.FexillonTwin/Controllers/SpatialController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xbim.Flex.Client;
+using Xbim.Flex.DI.Extensions.Services;
+
+namespace Xbim.Demo.FexillonTwin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpatialController : ControllerBase
+    {
+        private readonly ILogger<SpatialController> _logger;
+        private readonly IFlexClientsProvider flexClientsProvider;
+        private readonly IFlexAdmin admin;
+        private readonly IFlexAdminClientsProvider flexAdminClientsProvider;
+
+        public SpatialController(ILogger<SpatialController> logger, IFlexClientsProvider flexClientsProvider, IFlexAdmin admin, IFlexAdminClientsProvider flexAdminClientsProvider)
+        {
+            _logger = logger;
+            this.flexClientsProvider = flexClientsProvider;
+            this.admin = admin;
+            this.flexAdminClientsProvider = flexAdminClientsProvider;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Space>> Get()
+        {
+
+            var flexIdentityApi = await flexAdminClientsProvider.GetFlexIdentityAPIAsync();
+
+            var tenants = await flexIdentityApi.Tenants_GetAsync();
+
+
+            var flexApi = await flexAdminClientsProvider.GetFlexAIMAPIAsync();
+
+
+            var result = await flexApi.Spaces_GetAsync("microsoft", Region.Sandbox);
+
+            return result.Value;
+            
+        }
+    }
+}
---
+++ Xbim.Demo.FexillonTwin/Helpers/FlexServiceExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace Xbim.Demo.FexillonTwin.Helpers
+{
+    public static class FlexServiceExtensions
+    {
+		/// <summary>
+		/// Adds log-in services based on the configuration. It will either be
+		/// an interactive log-in service or non-interactive service using local directory or ldap identity
+		/// </summary>
+		/// <param name="config">Configuration</param>
+		/// <param name="services">Services</param>
+		public static IServiceCollection AddFlexLogInServices(this IServiceCollection services, IConfiguration config)
+		{
+			
+			var flexSection = config.GetSection("Flex");
+			if (flexSection != null)
+			{
+				services.AddXbimFlex(config)
+					.AddAimAPI()
+					.AddIdentityAPI();
+				// spoof IAuthorizationService
+				//services.AddSingleton<IAuthorizationService, NullAuthorizationService>();
+			}
+			else
+			{
+				//// this admin service will always throw as it is only available in the SSO/directory mode
+				//services.AddSingleton<IFlexAdmin, NullFlexAdminService>();
+
+			}
+
+			//if (string.IsNullOrWhiteSpace(config["DirectoryMode"]) || IsOneTimeFlexLogIn())
+			//{
+			//	// using implicit Flex flow and identities
+			//	services.AddSingleton<ILogInService, DefaultLoginService>();
+			//	// using DefaultLoginService fo get authenticated clients
+			//	services.ReplaceTransient<IFlexClientsProvider, DefaultFlexClientsProvider>();
+			//}
+			//else
+			//{
+			//	// directory based login service
+			//	services.AddSingleton<ILogInService, DirectoryLogInService>();
+
+			//}
+
+			//if (string.Equals(config["DirectoryMode"] ?? string.Empty, "directory", StringComparison.OrdinalIgnoreCase))
+			//{
+			//	// replace profile service to use directory information fot the current user
+			//	services.ReplaceTransient<IFlexUserProfileProvider, DirectoryFlexUserProfileProvider>();
+			//}
+			//else
+			//{
+			//	services.ReplaceTransient<IFlexUserProfileProvider, NullFlexUserProfileProvider>();
+			//}
+
+			return services;
+		}
+	}
+}
